fd99048acc8bd5eb20ef284bc4210b9b1275d1c5
sihamelghazy <siham.elghazy@proton.me>
2025-12-30 03:04:52 +0100

    Remove fun keyword from BrochuresApi interface

--- feature/brochures/src/main/java/com/bonial/feature/brochures/data/remote/BrochuresApi.kt
+++ feature/brochures/src/main/java/com/bonial/feature/brochures/data/remote/BrochuresApi.kt
@@ -3,7 +3,7 @@ package com.bonial.feature.brochures.data.remote
 import com.bonial.feature.brochures.data.remote.model.ShelfResponseDto
 import de.jensklingenberg.ktorfit.http.GET
 
-fun interface BrochuresApi {
+interface BrochuresApi {
     @GET("shelf.json")
     suspend fun getShelf(): ShelfResponseDto
 }
